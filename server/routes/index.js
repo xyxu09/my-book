08e1f4ded1674a2413aea818dab0d8767ec4b420
liuxs <907478372@qq.com>
2019-04-20 15:54:19 +0800

    login-->jscode2session获取code实现登入授权的流程

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,4 +32,7 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+// 自己用来处理登入的
+router.get('/myLogin', controllers.myLogin)
+
 module.exports = router
